edf6e396ce96eb940c1d2998fe6fc0352da1c24b
Greg s Pask <gregp63@hotmail.co.uk>
2018-11-19 17:54:09 +0000

    Updates

---
+++ precache-manifest.c7d19f74782fe2dca122d3d103f59219.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9c7fa086955b76e0899",
+    "url": "/React-JS-Snake-liveVersion/static/js/runtime~main.c9c7fa08.js"
+  },
+  {
+    "revision": "04bece176c3e38457fbe",
+    "url": "/React-JS-Snake-liveVersion/static/js/main.04bece17.chunk.js"
+  },
+  {
+    "revision": "8c0859d6a58d61cc3a39",
+    "url": "/React-JS-Snake-liveVersion/static/js/1.8c0859d6.chunk.js"
+  },
+  {
+    "revision": "04bece176c3e38457fbe",
+    "url": "/React-JS-Snake-liveVersion/static/css/main.4fc1c096.chunk.css"
+  },
+  {
+    "revision": "549aba1bddd4e1467db2ad0221b2ade7",
+    "url": "/React-JS-Snake-liveVersion/index.html"
+  }
+];
